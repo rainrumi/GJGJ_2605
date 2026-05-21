a5916259a9367c74b43dd1c0a259c8ef70591ab3
辻友斗 <NCR2519100006@edu.nca.ac.jp>
2026-05-21 11:47:25 +0900

    各ステージをタグで名称を指定

--- data/definitions/stage_definition.gd
+++ data/definitions/stage_definition.gd
@@ -23,8 +23,6 @@ enum StageArea {
 @export var level_text := ""
 @export var location := ""
 @export var reward_icon: Texture2D
-@export var is_rare := false
-@export var tags: Array[StringName] = []
 @export var enemy_pool: Array[EnemyDefinition] = []
 @export var stomach_columns := 4
 @export var stomach_rows := 5
@@ -32,15 +30,3 @@ enum StageArea {
 
 func get_difficulty_text() -> String:
 	return "Lv.%d" % difficulty_level
-
-
-func get_tag_text() -> String:
-	var tag_texts: Array[String] = []
-	if is_rare:
-		tag_texts.append("レア")
-	for tag in tags:
-		if not String(tag).is_empty():
-			tag_texts.append(String(tag))
-	if tag_texts.is_empty():
-		return "通常"
-	return " / ".join(tag_texts)
