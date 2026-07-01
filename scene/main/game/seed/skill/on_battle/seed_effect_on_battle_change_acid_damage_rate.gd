a40b52e7f57bb1d0e909a8badbc5e281917ef336
MAINMAPC\02rak <0>
2026-07-01 16:50:44 +0900

    変動消化ダメージ量の上限と下限を削除

--- scene/main/game/seed/skill/on_battle/seed_effect_on_battle_change_acid_damage_rate.gd
+++ scene/main/game/seed/skill/on_battle/seed_effect_on_battle_change_acid_damage_rate.gd
@@ -2,9 +2,8 @@ class_name SeedEffectOnBattleChangeAcidDamageRate
 extends SeedEffect
 
 @export var rate := 0.0 # 酸倍率
-@export var max_rate := 999.0 # 上限率
 
 
 # 消化率取得
 func get_acid_damage_rate(_state: DreamSeedSkillState, _context: Dictionary) -> float:
-	return clampf(rate, -max_rate, max_rate)
+	return rate
