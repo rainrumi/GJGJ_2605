9027f5d5513f66138abc6200a35d283d3ddd6491
ZBOOK-03\cre <.>
2026-07-15 14:41:11 +0900

    infoの内容を書き換え？

--- data/info/block/acid_block_info.gd
+++ data/info/block/acid_block_info.gd
@@ -4,7 +4,7 @@ extends Resource
 @export var max_hp := 1
 @export var damage := 0
 @export var texture: Texture2D
-@export var stomach_shape: Array[Vector2i] = [Vector2i.ZERO]
+@export var stomach_shape: Array[PackedInt32Array] = [PackedInt32Array([1])]
 
 
 # 最大HP取得
@@ -21,9 +21,10 @@ func get_damage() -> int:
 func get_stomach_shape() -> Array[Vector2i]:
 	# 形状
 	var shape: Array[Vector2i] = []
-	for cell in stomach_shape:
-		if cell is Vector2i:
-			shape.append(cell)
+	for y in stomach_shape.size():
+		for x in stomach_shape[y].size():
+			if stomach_shape[y][x] != 0:
+				shape.append(Vector2i(x, y))
 	if shape.is_empty():
 		shape.append(Vector2i.ZERO)
 	return shape
